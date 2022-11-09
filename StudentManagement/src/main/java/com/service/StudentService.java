430aa70c77b480088b321e8ad41f84ea5cc020c5
Meet Mevada <66245947+mmk161898@users.noreply.github.com>
2022-11-10 00:18:31 +0530

    Added Code for Lab 6

---
+++ StudentManagement/src/main/java/com/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.service;
+
+import java.util.List;
+import com.entity.Student;
+
+public interface StudentService {
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student thestudent);
+
+	public void deleteById(int theId);
+
+}
